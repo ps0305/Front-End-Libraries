d6f85d437e46b3d3088048da99df8427cb4fca7c
Pankaj Singh <singh.pankaj@live.com>
2018-08-24 20:06:18 +0530

    Update ex01.js

--- React/ex01.js
+++ React/ex01.js
@@ -1,4 +1,4 @@
-let elements = [
+let arr = [
     { text: 'ES6 is awesome', type: 'p' },
     { text: 'ES6 is significant update in JS history', type: 'li' },
     { text: 'const creates immutable variables', type: 'li' },
@@ -6,8 +6,8 @@ let elements = [
     { text: 'ES6 was released in 2015', type: 'h3' }
 ];
 
-let bullets = elements.filter(ele => ele.type === 'li');
-for (bullet of bullets) {
-    let { text, type } = bullet;
+let contents = elements.forEach(data => data.type === 'li');
+for (content of contents) {
+    let { text, type } = contents;
     console.log(`Element type is ${type} with text ${text}`);
 }
