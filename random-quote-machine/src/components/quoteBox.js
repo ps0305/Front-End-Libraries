3d60184fe88c2b8b97f615bf0d89bdb968282111
ps0305 <singh.pankaj@live.com>
2018-09-26 17:54:45 +0530

    commit

--- random-quote-machine/src/components/quoteBox.js
+++ random-quote-machine/src/components/quoteBox.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import getQuote from '../actions/actions'
 import PropTypes from 'prop-types';
+import Header from './Header';
+import Footer from './Footer'
+import { connect } from 'react-redux'
 
 class Quote extends React.Component {
     static propTypes = {
@@ -18,13 +21,88 @@ class Quote extends React.Component {
     componentDidMount() {
         this.props.getQuote();
     }
-
-    render(){
-        return(
-           <Header/>
-           
-        )
+    refreshQuote = event => {
+        event.preventDefault();
+        this.props.getQuote();
+      };
+      renderQuote() {
+        const { error, loading, quote } = this.props;
+        let jsx = null;
+    
+        if (loading) {
+          jsx = (
+            <Quote.Text id="text" loading>
+              Loading...
+            </Quote.Text>
+          );
+        } else if (error) {
+          jsx = (
+            <Quote.Text id="text" error>
+              {error}
+            </Quote.Text>
+          );
+        } else {
+          jsx = (
+            <Fragment>
+              <Quote.Text id="text">{quote.text}</Quote.Text>
+              <Quote.Author id="author">{quote.author}</Quote.Author>
+            </Fragment>
+          );
+        }
+    
+        return jsx;
+      }
+    
+      render() {
+        const { error, loading, quote } = this.props;
+    
+        return (
+            <div>
+            <Header>
+              <Header.Logo />
+              <Header.Text>QuoteMe Machine</Header.Text>
+            </Header>
+            <QuoteContainer loading={loading}>
+              <Quote>{this.renderQuote()}</Quote>
+              <ButtonContainer>
+                <ButtonContainer.Button>
+                  <a id="new-quote" title="Retrieve a new quote" onClick={this.refreshQuote}>
+                  </a>
+                </ButtonContainer.Button>
+                <ButtonContainer.Button twitter>
+                  {!loading && error === '' ? (
+                    <a
+                      href={`https://twitter.com/intent/tweet?text=${encodeURI(
+                        `${quote.author} once said: ${quote.text}`
+                      )}&hashtags=${encodeURI(quote.category)}&via=quotememachine`}
+                      id="tweet-quote"
+                      title={`Tweet this ${quote.author} quote`}
+                      target="_blank"
+                      rel="noopener"
+                    >
+                     {/*<FontAwesomeIcon icon={faTwitter} aria-label="Tweet Quote" />*/}
+                    </a>
+                  ) : (
+                    <a id="tweet-quote" className="disabled">
+                      <FontAwesomeIcon icon={faTwitter} aria-hidden="true" />
+                    </a>
+                  )}
+                </ButtonContainer.Button>
+              </ButtonContainer>
+            </QuoteContainer>
+            <Footer>
+              <p>
+                <a href="https://github.com/ps0305">
+                  Made with <span>♥</span> by Pankaj Singh
+                </a>
+              </p>
+            </Footer>
+            </div>
+        );
+      }
     }
-}
-
-export default Quote;
+    
+    export default connect(
+      state => state,
+      { getQuote }
+    )(QuoteBox);
