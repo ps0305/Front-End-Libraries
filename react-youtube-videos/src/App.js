88d0bebb97f5af2f5d95cad7157faa9da8684fe6
ps0305 <singh.pankaj@live.com>
2018-10-12 19:28:48 +0530

    commit

--- react-youtube-videos/src/App.js
+++ react-youtube-videos/src/App.js
@@ -15,7 +15,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
-    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+    this.videoSearch('surfboards');
+   
+  }
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       //console.log(data);
       this.setState({
         videos: videos,
@@ -27,7 +31,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
--- react-youtube-videos/src/components/video_list.js
+++ react-youtube-videos/src/components/video_list.js
@@ -11,7 +11,7 @@ const VideoList = (props) => {
         )
     });
     return (
-        <ul className="col-md-4 collection-item">
+        <ul className="col-md-4 list-group">
            {videoItems} 
         </ul>
     )
