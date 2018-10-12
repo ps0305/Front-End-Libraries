3c68a4b69c105f9538ad4181cd05314bb6d3211b
ps0305 <singh.pankaj@live.com>
2018-10-12 16:27:15 +0530

    video fetched

--- react-youtube-videos/src/components/video_list_item.js
+++ react-youtube-videos/src/components/video_list_item.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const VideoListItem = (props) => {
-    return <li>Video</li>
+const VideoListItem = ({ video }) => {
+  //const video = props.video;=>instead doing this we can pass as props as above
+  //console.log(video);
+  const imageUrl = video.snippet.thumbnails.default.url;
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} alt="no result found" />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
