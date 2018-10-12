ebd64e94e21d88b5f42c8ba5ee3f32bc875c67f0
ps0305 <singh.pankaj@live.com>
2018-10-12 14:50:31 +0530

    yarn install

--- react-youtube-videos/src/App.js
+++ react-youtube-videos/src/App.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY = AIzaSyB3a9E4BzF3u_LpOhB8pkiYrfK3iYZNgHw;
+const API_KEY = 'AIzaSyB3a9E4BzF3u_LpOhB8pkiYrfK3iYZNgHw';
+
+YTSearch({key: API_KEY, term:'surfboards'}, function(data){
+  console.log(data)
+})
+
+
 
 class App extends Component {
-  render() {
+  render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SearchBar />
       </div>
     );
-  }
 }
+  }
+  
 
 export default App;
