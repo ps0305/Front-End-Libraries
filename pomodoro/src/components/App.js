3e8e885d2d698e4575a495f7a5bb62943da7f2dc
ps0305 <pankajsingh0305@gmail.com>
2018-10-24 19:25:00 +0530

    functionality

--- pomodoro/src/components/App.js
+++ pomodoro/src/components/App.js
@@ -5,13 +5,27 @@ import TimeController from "./TimeController";
 import Sound from "./Sound";
 
 class App extends Component {
+  constructor () {
+    super ()
+    this.state = {
+      cycle: "Session",
+      workTime: 25,
+      breakTime: 5,
+      sound: "on"
+    }
+  }
+  setSound = (sound) => {
+    this.setState({
+      sound: sound
+    })
+  }
   render() {
     return (
       <div className="main">
         <h1>Pomodoro Clock</h1>
         <Timer />
         <TimeController />
-        <Sound />
+        <Sound setSound={this.setSound} sound={this.state.sound} />
       </div>
     );
   }
