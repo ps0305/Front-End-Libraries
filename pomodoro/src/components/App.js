352090c2d6c0351dae8522129c32503e10d02214
ps0305 <pankajsingh0305@gmail.com>
2018-10-24 20:04:44 +0530

    commit

--- pomodoro/src/components/App.js
+++ pomodoro/src/components/App.js
@@ -4,6 +4,28 @@ import Timer from "./Timer";
 import TimeController from "./TimeController";
 import Sound from "./Sound";
 
+
+incrementWorkTime = () => {
+  this.setState({
+    workTime : this.state.workTime + 1
+  })
+}
+decrementWorkTime = () => {
+  this.setState({
+    workTime : this.state.workTime - 1
+  })
+}
+incrementBreakTime = () => {
+  this.setState({
+    breakTime : this.state.breakTime + 1
+  })
+}
+decrementBreakTime = () => {
+  this.setState({
+    breakTime : this.state.breakTime - 1
+  })
+}
+
 class App extends Component {
   constructor () {
     super ()
@@ -24,7 +46,13 @@ class App extends Component {
       <div className="main">
         <h1>Pomodoro Clock</h1>
         <Timer />
-        <TimeController />
+        <TimeController
+        workTime={this.state.workTime}
+        breakTime={this.state.breakTime}
+        incrementWorkTime={this.incrementWorkTime}
+        decrementWorkTime={this.decrementWorkTime}
+        incrementBreakTime={this.incrementBreakTime}
+        decrementBreakTime={this.decrementBreakTime} />
         <Sound setSound={this.setSound} sound={this.state.sound} />
       </div>
     );
