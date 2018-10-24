74c75947a7d60753c5b2fac35d844d6990ca8f57
ps0305 <pankajsingh0305@gmail.com>
2018-10-24 20:21:48 +0530

    breakcontroller functioning

--- pomodoro/src/components/BreakController.js
+++ pomodoro/src/components/BreakController.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 export default class BreakController extends Component {
+
+  handleBreakIncrement = () => {
+    this.props.incrementBreakTime()
+  }
+
+  handleBreakDecrement = () => {
+    this.props.decrementBreakTime()
+  }
+
   render() {
     return (
-      <div>
-        <button className="btn btn-outline-success"> + </button>
-        <span> /BREAKTIME/ </span>
-        <button className="btn btn-outline-warning"> - </button>
+      <div className="controller">
+      <p>BREAK</p>
+        <button className="btn btn-outline-success" 
+        onClick={this.handleBreakIncrement}> + </button>
+        <span> {this.props.breakTime} </span>
+        <button className="btn btn-outline-warning" 
+        onClick={this.handleBreakDecrement}> - </button>
       </div>
     );
   }
