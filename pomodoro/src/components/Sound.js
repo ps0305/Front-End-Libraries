276701446f2d9b5bfa1c61a07c2c5cc9c0d8cdfc
ps0305 <pankajsingh0305@gmail.com>
2018-10-24 19:28:06 +0530

    sound component functional

--- pomodoro/src/components/Sound.js
+++ pomodoro/src/components/Sound.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Sound extends Component {
+
+    toggleSound = () => {
+        this.props.sound === "on" ?
+        this.props.setSound("off") :
+        this.props.setSound("on")
+    }
     render() {
         return (
-            <button> SOUND </button>
+            <button onClick={this.toggleSound}> SOUND </button>
         )
     }
 }
