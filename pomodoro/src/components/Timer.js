91e34a908bcaea8bd187c18b841acc2c9843d396
ps0305 <pankajsingh0305@gmail.com>
2018-10-24 20:58:07 +0530

    methods to timer

--- pomodoro/src/components/Timer.js
+++ pomodoro/src/components/Timer.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 export default class Timer extends Component {
+
+  timer = () => {
+    if (this.props.timerRunning === true) {
+      clearInterval(this.props.timerId)
+      this.props.setCurrentTime("25 : 00")
+      this.props.setTimerRunning()
+    }
+    else{
+      this.props.cycle === "Session" ?
+      this.props.startTimer(this.props.workTime) :
+      this.props.startTimer(this.props.breakTime)
+    }
+  }
   render() {
     return (
       <div className="timer">
