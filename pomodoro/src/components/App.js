a1c6937aa810806c4ba508becfeedc2dd7dbd8f0
ps0305 <pankajsingh0305@gmail.com>
2018-10-24 21:20:58 +0530

    commit

--- pomodoro/src/components/App.js
+++ pomodoro/src/components/App.js
@@ -4,12 +4,9 @@ import Timer from "./Timer";
 import TimeController from "./TimeController";
 import Sound from "./Sound";
 
-
-
-
 class App extends Component {
-  constructor () {
-    super ()
+  constructor() {
+    super();
     this.state = {
       timerId: 0,
       timerRunning: false,
@@ -18,48 +15,48 @@ class App extends Component {
       workTime: 25,
       breakTime: 5,
       sound: "on"
-    }
-    
+    };
   }
-  setSound = (sound) => {
+  setSound = sound => {
     this.setState({
       sound: sound
-    })
-  }
+    });
+  };
 
   incrementWorkTime = () => {
     this.setState({
-      workTime : this.state.workTime + 1
-    })
-  }
+      workTime: this.state.workTime + 1
+    });
+  };
   decrementWorkTime = () => {
     this.setState({
-      workTime : this.state.workTime - 1
-    })
-  }
+      workTime: this.state.workTime - 1
+    });
+  };
   incrementBreakTime = () => {
     this.setState({
-      breakTime : this.state.breakTime + 1
-    })
-  }
+      breakTime: this.state.breakTime + 1
+    });
+  };
   decrementBreakTime = () => {
     this.setState({
-      breakTime : this.state.breakTime - 1
-    })
-  }
-  
+      breakTime: this.state.breakTime - 1
+    });
+  };
+
   render() {
     return (
       <div className="main">
         <h1>Pomodoro Clock</h1>
         <Timer />
         <TimeController
-        workTime={this.state.workTime}
-        breakTime={this.state.breakTime}
-        incrementWorkTime={this.incrementWorkTime}
-        decrementWorkTime={this.decrementWorkTime}
-        incrementBreakTime={this.incrementBreakTime}
-        decrementBreakTime={this.decrementBreakTime} />
+          workTime={this.state.workTime}
+          breakTime={this.state.breakTime}
+          incrementWorkTime={this.incrementWorkTime}
+          decrementWorkTime={this.decrementWorkTime}
+          incrementBreakTime={this.incrementBreakTime}
+          decrementBreakTime={this.decrementBreakTime}
+        />
         <Sound setSound={this.setSound} sound={this.state.sound} />
       </div>
     );
