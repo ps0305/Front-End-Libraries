229355e304615601d645eff2b53ad37d9be9749c
ps0305 <pankajsingh0305@gmail.com>
2018-10-24 19:58:19 +0530

    sound icon added

--- pomodoro/src/components/Sound.js
+++ pomodoro/src/components/Sound.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Sound extends Component {
-
-    toggleSound = () => {
-        this.props.sound === "on" ?
-        this.props.setSound("off") :
-        this.props.setSound("on")
-    }
-    render() {
-        return (
-            <button className="btn btn-primary" onClick={this.toggleSound}> SOUND </button>
+  toggleSound = e => {
+    this.props.sound === "on"
+      ? this.changeSoundAttributes(
+          "off",
+          process.env.PUBLIC_URL + "/assets/sound.png",
+          e
         )
-    }
+      : this.changeSoundAttributes(
+          "on",
+          process.env.PUBLIC_URL + "/assets/sound.png",
+          e
+        );
+  };
+
+  changeSoundAttributes = (toggle, src, e) => {
+    this.props.setSound(toggle);
+    e.target.src = src;
+  };
+  render() {
+    return (
+      <button
+        onClick={e => {
+          this.toggleSound(e);
+        }}
+      >
+        <img src={process.env.PUBLIC_URL + "/assets/sound.png" } alt="sound" height="30px"/>
+      </button>
+    );
+  }
 }
