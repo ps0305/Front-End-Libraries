fc6e3cea12a74d9ae01621ae1e09678eb6e5ecab
ps0305 <pankajsingh0305@gmail.com>
2018-10-25 19:40:40 +0530

    commit

--- pomodoro/src/components/App.js
+++ pomodoro/src/components/App.js
@@ -5,8 +5,8 @@ import TimeController from "./TimeController";
 import Sound from "./Sound";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       timerId: 0,
       timerRunning: false,
@@ -15,13 +15,49 @@ class App extends Component {
       workTime: 25,
       breakTime: 5,
       sound: "on"
-    };
+    }
   }
   setSound = sound => {
     this.setState({
       sound: sound
     });
   };
+
+  startTimer = (duration) => {
+    this.setState({
+      timerRunning: true
+    })
+    let time = duration * 60
+    let minutes;
+    let seconds;
+    let runningTimer = setInterval(() => {
+      this.setState({
+        timerId: runningTimer
+      })
+      minutes = Math.floor(time / 60)
+      seconds = time - minutes * 60
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      this.setState({currentTime: `${minutes} : ${seconds}`})
+      if (time == 0) {
+        if(this.state.cycle === "Session"){
+          this.setState({
+            cycle: "Break",
+            timerRunning: false
+          })
+          clearInterval(this.state.timerId)
+          this.startTimer(this.state.breakTime)
+        }else{
+          this.setState({
+            cycle: "Session",
+            timerRunning: false
+          })
+          clearInterval(this.state.timerId)
+          this.startTimer(this.state.workTime)
+        }
+      }
+    },1000);
+  }
 
   incrementWorkTime = () => {
     this.setState({
